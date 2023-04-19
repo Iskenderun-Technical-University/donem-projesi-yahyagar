ffd2ed74b99a03b4830119cd3744df7ba06c2d27
=Yahya Ağar <=yahya.agarr@gmail.com>
2023-04-20 00:37:31 +0300

    giris ekrani ve ana ekran eklendi

---
+++ disKlinigi/disKlinigi/login.cs
@@ -0,0 +1,15 @@
+namespace disKlinigi
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
